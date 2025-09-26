56934adcd474f2134ad82a991784410e0d79c877
afterresultsolutions <afterresult.solutions@gmail.com>
2025-09-27 02:22:34 +0530

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -10,13 +10,12 @@ const menuitems = [
   },
 ];
 </script>
-
 <template>
   <Container>
     <header class="flex flex-row flex-wrap items-center justify-between my-4 gap-x-6 gap-y-4">
       <div class="flex items-center justify-between">
         <a href="/" class="text-2xl font-bold text-zinc-800">
-          <span class="text-green-500">&#10070;</span> Nuxt Starter
+          <span class="text-green-500">&#10070;</span> AR Solutions
         </a>
       </div>
       <nav class="flex w-auto">
