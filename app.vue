cca6f08319a04ead4f7a2f94d7c30ce01c57d6f3
afterresultsolutions <afterresult.solutions@gmail.com>
2025-09-27 02:14:35 +0530

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,12 +1,12 @@
 <script setup>
 useHead({
   htmlAttrs: { lang: "en-US" },
-  title: "Nuxt Starter",
+  title: "AR Solutions",
   meta: [
     {
       name: "description",
       content:
-        "Nuxt Starter is a minimal template created for quick landing pages, built with Nuxt and Tailwind CSS.",
+        "Helping You Scale Smarter While Maximizing ROI.",
     },
   ],
   bodyAttrs: {
