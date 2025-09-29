3d4d190475892a0f486f95b13e6150ed0e0cca45
afterresultsolutions <afterresult.solutions@gmail.com>
2025-09-29 09:12:58 +0530

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,17 +6,18 @@ const menuitems = [
   },
 ];
 </script>
+
 <template>
   <Container>
     <header class="flex flex-row flex-wrap items-center justify-between my-4 gap-x-6 gap-y-4">
       <div class="flex items-center justify-between">
         <a href="/" class="text-2xl font-bold text-zinc-800">
-          <span class="text-green-500">&#10070;</span> AR Solutions
+          AR Solutions
         </a>
       </div>
       <nav class="flex w-auto">
         <ul class="flex flex-row gap-3 text-sm">
-          <li v-for="item of menuitems">
+          <li v-for="item of menuitems" :key="item.title">
             <a :href="item.url" class="flex px-4 py-1 text-white bg-green-500 rounded-full text-sm hover:bg-green-600">
               {{ item.title }}
             </a>
@@ -24,5 +25,4 @@ const menuitems = [
         </ul>
       </nav>
     </header>
-  </Container>
-</template>
+  </Cont
