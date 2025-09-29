972b144206b2b28f3b0d5d5f97bdc9e3305d1215
afterresultsolutions <afterresult.solutions@gmail.com>
2025-09-29 09:17:13 +0530

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,28 +1,28 @@
 <script setup>
 const menuitems = [
   {
-    title: "AfterResult AI",
-    url: "https://afterresult.solutions/AI",
+    title: "AI Chatbot",
+    url: "https://afterresult.solutions/ai/",
   },
 ];
 </script>
-
 <template>
   <Container>
     <header class="flex flex-row flex-wrap items-center justify-between my-4 gap-x-6 gap-y-4">
       <div class="flex items-center justify-between">
         <a href="/" class="text-2xl font-bold text-zinc-800">
-          AR Solutions
+          <span class="text-white-500">&#10070;</span> AR Solutions
         </a>
       </div>
       <nav class="flex w-auto">
         <ul class="flex flex-row gap-3 text-sm">
-          <li v-for="item of menuitems" :key="item.title">
-            <a :href="item.url" class="flex px-4 py-1 text-white bg-green-500 rounded-full text-sm hover:bg-green-600">
+          <li v-for="item of menuitems">
+            <a :href="item.url" class="flex px-4 py-1 text-white bg-orange-500 rounded-full text-sm hover:bg-orange-600">
               {{ item.title }}
             </a>
           </li>
         </ul>
       </nav>
     </header>
-  </Cont
+  </Container>
+</template>
