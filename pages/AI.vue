26bc870cf0d3e69f5b17cd64bfa0bdcd677e05c6
afterresultsolutions <afterresult.solutions@gmail.com>
2025-09-29 09:43:38 +0530

    Update AI.vue

--- pages/AI.vue
+++ pages/AI.vue
@@ -119,7 +119,7 @@
                 </button>
               </div>
               <div :class="['disclaimer', isDarkMode ? 'text-gray-500' : 'text-gray-400']">
-                AR Solutions AI can make mistakes. Check important info.
+                “AR Solutions AI, in its pre-build phase, is currently handling only ecommerce queries”.
               </div>
             </div>
           </div>
