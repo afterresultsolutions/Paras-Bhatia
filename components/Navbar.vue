0a5777a52b919c56ffc928a52bba7b0cfdba54b2
afterresultsolutions <afterresult.solutions@gmail.com>
2025-09-29 09:19:45 +0530

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -10,7 +10,7 @@ const menuitems = [
   <Container>
     <header class="flex flex-row flex-wrap items-center justify-between my-4 gap-x-6 gap-y-4">
       <div class="flex items-center justify-between">
-        <a href="/" class="text-2xl font-bold text-zinc-800">
+        <a href="/" class="text-2xl font-bold text--800">
           <span class="text--500">&#10070;</span> AR Solutions
         </a>
       </div>
