4af44ff527041f3f2f06014c433e4bcfd5a89730
afterresultsolutions <afterresult.solutions@gmail.com>
2025-09-29 09:21:21 +0530

    Update Logos.vue

--- components/Logos.vue
+++ components/Logos.vue
@@ -1,7 +1,7 @@
 <script setup>
 const props = defineProps({
   title: String,
-  icons: Array
+  icons: 
 });
 </script>
 
@@ -9,7 +9,7 @@ const props = defineProps({
   <div class="mb-20">
     <h2 class="text-center text-zinc-600" v-if="title">{{ title }}</h2>
     <div class="flex flex-wrap items-center justify-center mt-10 gap-x-12 gap-y-8 sm:gap-x-24 sm:gap-y-12">
-      <div v-for="icon of icons">
+      <div v-for="">
         <Icon class="h-8 md:h-12" v-if="icon.name" :name="icon.name" size="48" />
       </div>
     </div>
