d464da3d24598c523295f915cb773e485b8a4c6e
afterresultsolutions <afterresult.solutions@gmail.com>
2025-09-29 07:06:19 +0530

    Update live.vue

--- pages/live.vue
+++ pages/live.vue
@@ -8,7 +8,7 @@ definePageMeta({
   <div class="flex flex-col items-center justify-center min-h-screen text-center p-8">
     <h1 class="text-4xl font-bold mb-4">We Are Live 🚀</h1>
     <p class="text-lg text-gray-600 max-w-xl">
-      Grow Faster with AfterResult Solutions - Your Trusted Digital Marketing Agency AfterResult Solutions is a full-service marketing agency focused on helping businesses grow through a blend of digital strategies, branding, automation, and outreach-both online and offline. We work with startups, SMEs, anc established brands to deliver tailored marketing solutions that are built for scalability and long-term results. Our approach is structured, data-driven, and performance-focused By integrating design, technology, and strategic execution, we enable brands to build a powerful digital presence, generate leads consistently, improve visibility in local markets, and scale efficiently. Whether you're a startup looking to launch, or an established business aiming to expand your reach, AfterResult provides the systems and support you need to grow with confidence.<b>/live</b>.
+      Grow Faster with AfterResult Solutions - Your Trusted Digital Marketing Agency AfterResult Solutions is a full-service marketing agency <b>/live</b>.
     </p>
   </div>
 </template>
